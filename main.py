fca2eb37db4c53fde2311565f68669f798b715a5
supascooopa <mnalptekin1@gmail.com>
2023-07-14 11:50:26 -0700

    adding text

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-# amend these 🖕🖕
+# amend these blah blah blah
 x = "hello"
 y = "!"
 print(x + " " + y)
 
 # this is before the revert
+"""
+blah blah blah
+blah blah blah
+blah blah blah
+blah blah blah
+blah blah blah
+
+"""
